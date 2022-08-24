6e24301d1d4ec20be5092b761ceb3af861ed5754
atharvacharya <88115500+atharvacharya@users.noreply.github.com>
2022-08-24 16:46:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************
+* Hello.Java
+*************************************************
+* Atharv Acharya
+* 8/24/2022
+* CMSC 255
+*************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Atharv>");
+		System.out.println("<Hello.Java>");
+		System.out.println("<Write your name in Java>");
+		System.out.println("<8/24/2022>");
+		System.out.println("<CMSC 255>");
+		System.out.println();
+		}
+	}
+	
